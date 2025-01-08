d48b789049177f502a5e6b86380936dfeca5ef29
AdrianRuiz <arues20@gmail.com>
2025-01-08 15:40:07 +0100

    Implementado sonido en jugador

--- Assets/Project/Scripts/Enemies/Detection/EnemyStates.cs
+++ Assets/Project/Scripts/Enemies/Detection/EnemyStates.cs
@@ -6,7 +6,7 @@ public class EnemyStates
 {
     public enum FOVState
     {
-        isWatching,
+        isUnaware,
         isDetecting,
         isSeeing,
     }
--- Assets/Project/Scripts/NoiseSystem/NoiseCircle.cs
+++ Assets/Project/Scripts/NoiseSystem/NoiseCircle.cs
@@ -2,39 +2,50 @@ using UnityEngine;
 
 namespace Player
 {
-   public class NoiseCircle : MonoBehaviour
-{
-
-    private CircleCollider2D circleCollider;
-
-    private float currentRadius;
-    
-
-    [Header("RADIUS")]
-
-    [SerializeField] private bool worldNoiseIsActivated;
-    
-    void Start()
-    { 
-        circleCollider = GetComponent<CircleCollider2D>();
-    }
-
-    private void Update()
+    public class NoiseCircle : MonoBehaviour
     {
-        currentRadius = PlayerController.Instance.GetCurrentRadius();
-        this.circleCollider.radius = currentRadius;
-    }
-    
-    public void UpdateColliderOnWorldNoise()
-    {
-        worldNoiseIsActivated = true;
+
+        private CircleCollider2D circleCollider;
+        private PlayerController playerController;
+
+        private float currentRadius;
+
+
+        [Header("RADIUS")]
+
+        [SerializeField] private bool worldNoiseIsActivated;
+
+        void Start()
+        {
+            playerController = GetComponentInParent<PlayerController>();
+            circleCollider = GetComponent<CircleCollider2D>();
+        }
+
+        private void Update()
+        {
+            currentRadius = PlayerController.Instance.GetCurrentRadius();
+            this.circleCollider.radius = currentRadius;
+
+            if (playerController.GetMoveSpeed() <= 0 || NoiseManager.Instance.GetIfWorldNoiseIsActivated())
+            {
+                circleCollider.enabled = false;
+            }
+            else
+            {
+                circleCollider.enabled = true;
+            }
+        }
+
+        public void UpdateColliderOnWorldNoise()
+        {
+            worldNoiseIsActivated = true;
+        }
+
+        public void ResetColliderOnWorldNoise()
+        {
+            worldNoiseIsActivated = false;
+        }
+
     }
 
-    public void ResetColliderOnWorldNoise()
-    {
-        worldNoiseIsActivated = false;
-    }
-
-}
- 
 }
